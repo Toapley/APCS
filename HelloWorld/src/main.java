0bcfc03103459e922fabf2bf2d8f47aab23d47f0
toapley <toapley@482L072172.lwsd.k12wa.org>
2018-02-20 16:16:53 -0800

    dd

--- HelloWorld/src/main.java
+++ HelloWorld/src/main.java
@@ -1,11 +1,33 @@
+import java.util.*;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hi mom");
-		System.out.println("What is gong on?");
-		System.out.println("yippee");
+
+	       ArrayList<String> l = new ArrayList<String>();
+	       l.add("Mom");
+	       l.add("Dad");
+	       l.add("bo");
+	       System.out.println(l);
+	       
+	       
+	       for(int i = 0; i < l.size(); i++) {
+	              String s = l.get(i);
+	              l.add(i + 1, s);
+	              System.out.println(l);
+	       } 
+	       
+	       /*
+	       for (int i = 0; i< l.size(); i++) {
+	              if (l.get(i).length() % 2 != 0 ) {
+	                    l.remove(i);
+	                    i--;
+	              }
+	       }
+	       */
+	       
+	       System.out.println(l);
+
 	}
 
 }
