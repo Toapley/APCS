de51900d7f446bd964cbbfec70479c2d9547a462
Todd <Todd@ToddXPS.home>
2018-09-11 06:14:56 -0700

    update name

--- ClassSorter/src/mainCode.java
+++ ClassSorter/src/mainCode.java
@@ -6,8 +6,8 @@ public class mainCode {
 						
 		String[] period1 = {"Trixie", "Susan", "Annika", "Connor", "Hayden", "James", "John", "Jenna", "Shira", "Jarec", "Gavin", "Maggie", "Alyssa", "Harris", "Garrett", "Faraz", "Pranathi", "Brandon", "Ajay", "Casey", "Kevin", "Fiona"};
 		String[] period2 = {"Bryce", "Xavier", "Andreea", "Mark", "Tianyi", "Nathan", "Sean", "Ivan", "Sanjana", "Melvin", "Spencer"};
-		String[] period3 = {"Anna", "Dylan", "Jake", "Jaron","Cate","Landon","Koji","Adnan","Michaela","James","Andrey","Trotsky","Sim","Joshua","Ben","Oliver","Morgan","Grant","Kokoa","Calin","Simon","Daniel","David","Cameron","Drew","Caden","Oscar","Grayson","Afrah"};
-		String[] period4 = {"Aidan A", "Tyler", "Terence", "Sadie", "Adrian", "Mitchell", "Natalie", "Taylor", "Thomas", "Arya", "Landon", "Ann", "Luc", "Dovovan H", "Kenny", "Julian", "Sonya", "Kelson", "Nikita", "Emily", "Donovan L", "Mauricio", "Dora", "Danny", "Aiden S", "Harrison", "Gloria", "Indigo", "Kiara", "Amara"};
+		String[] period3 = {"Anna", "Dylan", "Jake", "Jaron","Cate","Landon","Koji","Adnan","Michaela","James","Andrey","Trotsky","Sim","Joshua","Ben","Oliver","Morgan","Grant","Kokoa","Calin","Simon","Daniel","David","Cameron","Drew","Caden","Oscar","Grayson","Afrah", "Nate", "Sophia"};
+		String[] period4 = {"Aidan A", "Tyler", "Terence", "Sadie", "Adrian", "Mitchell", "Natalie", "Taylor", "Thomas", "John", "Landon", "Ann", "Luc", "Dovovan H", "Kenny", "Julian", "Sonya", "Kelson", "Nikita", "Emily", "Donovan L", "Mauricio", "Dora", "Danny", "Aiden S", "Harrison", "Gloria", "Indigo", "Kiara", "Amara"};
 		sortClass(period3,2);	
 	}
 	
