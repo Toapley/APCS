224c687d3d576e1e1c5297440d9421f92d11ae2b
Todd <Todd@ToddXPS.home>
2018-09-13 16:26:00 -0700

    Added figures project

---
+++ Figures/src/Figures.java
@@ -0,0 +1,30 @@
+public class Figures {
+	
+	/* TODO: Use procedural decomposition to create structure and
+	 * eliminate redundancy   
+	 */
+	public static void main(String[] args) {
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println("/        \\");
+		System.out.println("\\        /");
+		System.out.println(" \\______/");
+		System.out.println();
+		System.out.println("\\        /");
+		System.out.println(" \\______/");
+		System.out.println("+--------+");
+		System.out.println();
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println("/        \\");
+		System.out.println("|  STOP  |");
+		System.out.println("\\        /");
+		System.out.println(" \\______/");
+		System.out.println();
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println("/        \\");
+		System.out.println("+--------+");
+	}
+	
+}
