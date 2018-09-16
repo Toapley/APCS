197139611597c90b96443f3c571790d40a599642
Todd <Todd@ToddXPS.home>
2018-09-16 07:01:25 -0700

    Updated names

--- ClassSorter/src/mainCode.java
+++ ClassSorter/src/mainCode.java
@@ -4,11 +4,11 @@ public class mainCode {
 
 	public static void main(String[] args) {
 						
-		String[] period1 = {"Trixie", "Susan", "Annika", "Connor", "Hayden", "James", "John", "Jenna", "Shira", "Jarec", "Gavin", "Maggie", "Alyssa", "Harris", "Garrett", "Faraz", "Pranathi", "Brandon", "Ajay", "Casey", "Kevin", "Fiona"};
+		String[] period1 = {"Trixie", "Susan", "Annika", "Connor", "Hayden", "James", "John", "Jenna", "Shira", "Jarec", "Gavin", "Maggie", "Alyssa", "Harris", "Garrett", "Faraz", "Pranathi", "Brandon", "Ajay", "Casey", "Fiona"};
 		String[] period2 = {"Bryce", "Xavier", "Andreea", "Mark", "Tianyi", "Nathan", "Sean", "Ivan", "Sanjana", "Melvin", "Spencer"};
 		String[] period3 = {"Anna", "Dylan", "Jake", "Jaron","Cate","Landon","Koji","Adnan","Michaela","James","Andrey","Trotsky","Sim","Joshua","Oliver","Morgan","Grant","Kokoa","Calin","Simon","Daniel","David","Cameron","Drew","Caden","Oscar","Grayson","Afrah", "Nate", "Sophia"};
 		String[] period4 = {"Aidan A", "Tyler", "Terence", "Sadie", "Adrian", "Mitchell", "Natalie", "Taylor", "Thomas", "John", "Landon", "Ann", "Luc", "Dovovan H", "Kenny", "Julian", "Sonya", "Kelson", "Nikita", "Emily", "Donovan L", "Mauricio", "Dora", "Danny", "Aiden S", "Harrison", "Gloria", "Indigo", "Kiara", "Amara", "Ayden R"};
-		sortClass(period4,1);	
+		sortClass(period3,1);	
 	}
 	
 	// Given a list of people in the class and the size of groups desired
