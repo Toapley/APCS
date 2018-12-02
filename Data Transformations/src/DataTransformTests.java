476b774d7bbce092087c15dff6b953e8965c5772
Todd Apley <Toapley@lwsd.org>
2018-12-01 16:01:41 -0800

    DataTranformations Lab

---
+++ Data Transformations/src/DataTransformTests.java
@@ -0,0 +1,220 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class DataTransformTests {
+
+	@Test
+	void testMaxWordLengthSimple() {
+
+		// Create a test array
+		String input[] = { "Beet" };
+		int expected = 4;
+
+		// Use assertEquals to compare numbers;
+		assertEquals(expected, Main.maxWordLength(input));
+
+	}
+
+	@Test
+	void testwordLengthsTallyHandout2() {
+
+		// This is from the handout.
+		String input = Main.TEXT_2;
+		String expected[] = {"A","long","time","ago","in","a","galaxy","far,","far","away..."};
+		
+		// Use assertEquals to compare arrays
+		assertArrayEquals(expected, Main.splitString(input) );
+		
+		assertEquals(7, Main.maxWordLength(Main.splitString(input)));
+
+	}
+	
+	@Test
+	void testwordLengthsTallyHandout1() {
+
+		// This is from the handout.
+		String input = Main.TEXT_1;
+		String expected[] = {"Star","Wars","Episode","VII","The","Force","Awakens"};
+		
+		// Use assertEquals to compare arrays
+		assertArrayEquals(expected, Main.splitString(input) );
+
+	}
+	
+	@Test
+	void testSplitStringHandout() {
+
+		// This is from the handout.
+		String input[] = Main.TEXT_LIST;
+		int expected[] = {0, 9, 25, 42, 43, 26, 13, 11, 4, 2, 3, 1 };
+
+		// Use assertEquals to compare arrays
+		assertArrayEquals(expected, Main.wordLengthsTally(input) );
+
+	}
+
+	@Test
+	public void testIntersectWords_oneIntersection() {
+		String sentence1 = "aaa bbb ccc";
+		String sentence2 = "ccc ddd eee";
+		String[] expected = { "ccc" };
+		assertArrayEquals(expected, Main.intersectWords(sentence1, sentence2));
+	}
+
+	@Test
+	public void testIntersectWords_multipleIntersections() {
+		String sentence1 = "this is cool";
+		String sentence2 = "how cool is this";
+		String[] expected = { "this", "is", "cool" };
+		assertArrayEquals(expected, Main.intersectWords(sentence1, sentence2));
+	}
+	
+
+	@Test
+	public void testIntersectWords_Handout() {
+		String sentence1 = Main.TEXT_1;
+		String sentence2 = Main.TEXT_9;
+		String[] expected = {};
+		assertArrayEquals(expected, Main.intersectWords(sentence1, sentence2));
+	}
+
+	@Test
+	public void testIntersectWords_intersectionsAreUnique() {
+		String sentence1 = "this this is is is cool";
+		String sentence2 = "how cool is this";
+		String[] expected = { "this", "is", "cool" };
+		assertArrayEquals(expected, Main.intersectWords(sentence1, sentence2));
+	}
+
+	@Test
+	public void testIntersectWords_noIntersections() {
+		String sentence1 = "aaa bbb ccc";
+		String sentence2 = "ddd eee fff";
+		String[] expected = {};
+		assertArrayEquals(expected, Main.intersectWords(sentence1, sentence2));
+	}
+
+	@Test
+	public void testIntersectWords_emptyStrings() {
+		String sentence1 = "";
+		String sentence2 = "";
+		String[] expected = {};
+		assertArrayEquals(expected, Main.intersectWords(sentence1, sentence2));
+	}
+
+	@Test
+	public void testSplitString_oneWord() {
+		String[] expected = { "abc" };
+		assertArrayEquals(expected, Main.splitString("abc"));
+	}
+
+	@Test
+	public void testSplitString_twoWords() {
+		String[] expected = { "a", "b" };
+		assertArrayEquals(expected, Main.splitString("a b"));
+	}
+
+	@Test
+	public void testSplitString_moreWords() {
+		String[] expected = { "a", "quick", "brown", "fox!" };
+		assertArrayEquals(expected, Main.splitString("a quick brown fox!"));
+	}
+
+	@Test
+	public void testSplitString_emptyString() {
+		String[] expected = {};
+		assertArrayEquals(expected, Main.splitString(""));
+	}
+
+	@Test
+	public void testSplitString_extraSpaces() {
+		String[] expected = { "a", "b", "c" };
+		assertArrayEquals(expected, Main.splitString(" a  b   c   "));
+	}
+
+	@Test
+	public void testMaxWordLength_oneWord() {
+		String[] arr = { "abc" };
+		assertEquals(3, Main.maxWordLength(arr));
+	}
+
+	@Test
+	public void testMaxWordLength_twoWords() {
+		String[] arr1 = { "a", "bc" };
+		assertEquals(2, Main.maxWordLength(arr1));
+		String[] arr2 = { "ab", "c" };
+		assertEquals(2, Main.maxWordLength(arr2));
+	}
+
+	@Test
+	public void testMaxWordLength_twoWordsSameLength() {
+		String[] arr = { "abc", "def" };
+		assertEquals(3, Main.maxWordLength(arr));
+	}
+
+	@Test
+	public void testMaxWordLength_threeWords() {
+		String[] arr1 = { "a", "bc", "def" };
+		assertEquals(3, Main.maxWordLength(arr1));
+		String[] arr2 = { "a", "bcd", "ef" };
+		assertEquals(3, Main.maxWordLength(arr2));
+		String[] arr3 = { "abc", "de", "fgh" };
+		assertEquals(3, Main.maxWordLength(arr3));
+	}
+
+	@Test
+	public void testMaxWordLength_emptyList() {
+		String[] arr = {};
+		assertEquals(0, Main.maxWordLength(arr));
+	}
+
+	@Test
+	public void testMaxWordLength_emptyString() {
+		String[] arr = { "" };
+		assertEquals(0, Main.maxWordLength(arr));
+	}
+
+	@Test
+	public void testWordLengthsTally() {
+
+		String ONE = "a b c d";
+		String TWO = "ab cd ef";
+		String FOUR = "abc, defg";
+		String SIX = "abcde!";
+		String[] sentences = { ONE, TWO, FOUR, SIX };
+		int expected[] = {0,4,3,2,1};
+		assertArrayEquals(expected, Main.wordLengthsTally(sentences));
+
+	}
+
+	@Test
+	public void testGenerateNGrams_extraCredit_() {
+		String[] expected1 = { "a", "b", "c" };
+		assertArrayEquals(expected1, Main.generateNGrams("a b c", 1));
+		String[] expected2 = { "a", "b", "c", "a b", "b c" };
+		assertArrayEquals(expected2, Main.generateNGrams("a b c", 2));
+		String[] expected3 = { "a", "b", "c", "a b", "b c", "c d", "a b c", "b c d", "a b c d" };
+		assertArrayEquals(expected3, Main.generateNGrams("a b c d", 4));
+	}
+
+	@Test
+	public void testGenerateNGrams_extraCredit_limitWindowSize() {
+		String[] expected1 = { "a", "b", "c", "a b", "b c", "a b c" };
+		assertArrayEquals(expected1, Main.generateNGrams("a b c", 100));
+	}
+
+	@Test
+	public void testGenerateNGrams_extraCredit_emptyInput() {
+		String[] expected = {};
+		assertArrayEquals(expected, Main.generateNGrams("", 5));
+	}
+
+	@Test
+	public void testGenerateNGrams_extraCredit_invalidWindowSize() {
+		String[] expected = {};
+		assertArrayEquals(expected, Main.generateNGrams("a b c", 0));
+		assertArrayEquals(expected, Main.generateNGrams("a b c", -1));
+	}
+
+}
---
+++ Data Transformations/src/Main.java
@@ -0,0 +1,50 @@
+
+public class Main
+{
+    public static final String TEXT_1 = "Star Wars: Episode VII The Force Awakens";
+    public static final String TEXT_2 = "A long time ago in a galaxy far, far away..."; 
+    public static final String TEXT_3 = "There's been an awakening. Have you felt it? The Dark side, and the Light.";
+    public static final String TEXT_4 = "It's true. All of it. The Dark Side, the Jedi. They're real.";
+    public static final String TEXT_5 = "The Force is strong in my family. My father has it. I have it. My sister has it. You have that power, too.";
+    public static final String TEXT_6 = "I have to remind myself that some birds aren't meant to be caged. Their feathers are just too bright.";
+    public static final String TEXT_7 = "Remember Red, hope is a good thing, maybe the best of things, and no good thing ever dies.";
+    public static final String TEXT_8 = "Life is like a box of chocolates. You never know what you’re gonna get.";
+    public static final String TEXT_9 = "I think this is the beginning of a beautiful friendship";
+    public static final String TEXT_10 = "This is your last chance. After this, there is no turning back. "
+            + "You take the blue pill, the story ends, you wake up in your bed and believe whatever you want to believe. "
+            + "You take the red pill, you stay in Wonderland and I show you how deep the rabbit hole goes.";
+    
+    public static final String[] TEXT_LIST = {TEXT_1, TEXT_2, TEXT_3, TEXT_4, TEXT_5, TEXT_6, TEXT_7, TEXT_8, TEXT_9, TEXT_10};
+
+    public static void main(String[] args)
+    {
+        /* There's no need to implement anything in main for this assignment
+         * You can use the unit test to validate your work
+         */
+
+    }
+    
+    public static int maxWordLength(String[] list) {
+    	return 1;
+    }
+    
+    public static String[] splitString(String sentence) {
+    	return new String[5];
+    }
+    
+    public static String[] intersectWords( String sentence1, String sentence2) {
+    	return new String[5];
+    }
+    
+    //TODO: Note:  the [0] index in the return array should always contain 0.
+    public static int[] wordLengthsTally(String[] sentenceList) {
+    	return new int[5];
+    }
+    
+    public static String[] generateNGrams(String text, int size)  {
+    	return new String[5];
+    }
+    
+    
+
+}
