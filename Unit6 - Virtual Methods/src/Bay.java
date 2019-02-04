4d254280c26b3cd6a9e8c649302061d3bffedcb7
Todd Apley <Toapley@lwsd.org>
2019-02-04 06:14:17 -0800

    Exercise for Unit 6

---
+++ Unit6 - Virtual Methods/src/Bay.java
@@ -0,0 +1,10 @@
+public class Bay extends Lake {
+	public void method1() {
+		System.out.print("Bay 1 ");
+		super.method2();
+	}
+
+	public void method2() {
+		System.out.print("Bay 2 ");
+	}
+}
---
+++ Unit6 - Virtual Methods/src/MainCode.java
@@ -0,0 +1,14 @@
+
+public class MainCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pond var1 = new Bay();
+		Object var2 = new Ocean();
+
+		((Pond) var2).method3();
+
+
+	}
+
+}
